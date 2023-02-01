27d42cd6f1cfc65b65c14e9871422e086d9f0c55
Ashutosh <ashutosh.kumar6@mindtree.com>
2023-02-01 05:12:24 +0000

    added swagger

--- AzureIOTDevice/Program.cs
+++ AzureIOTDevice/Program.cs
@@ -3,11 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers().AddNewtonsoftJson();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
-//app.UseRouting();
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
